1c1e23f3579e621365e815ee24ba0b9a682c78f1
김지훈 <55314488+hoonori@users.noreply.github.com>
2024-05-04 19:48:23 +0900

    implement discord_message
    sends discord message, added a program start message at the start as a test & great use case.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import fetch_data
 import manage_info
+import report
 
 def main():
+    report.discord_message("Starting Financial-Manager")
     symbols = fetch_data.load_symbols()
     for symbol in symbols:
         fetch_data.fetch_and_save_stock_data(symbol)
---
+++ report.py
@@ -0,0 +1,12 @@
+from config_loader import config
+import datetime
+import requests
+
+DISCORD_WEBHOOK_URL = config['DISCORD_WEBHOOK_URL']
+
+def discord_message(msg):
+    """디스코드 메세지 전송"""
+    now = datetime.datetime.now()
+    message = {"content": f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] {str(msg)}"}
+    requests.post(DISCORD_WEBHOOK_URL, data=message)
+    print(message)
